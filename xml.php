33cb7507f7f376de8d7a49bc04833a25da31c679
Adnan-Bacic <adnan-bacic@hotmail.com>
2021-03-01 21:56:20 +0100

    changed toggle view to 2 pages

--- xml.php
+++ xml.php
@@ -28,7 +28,32 @@
 		}
 	}
 ?>
-	<div class="container d-none" id="xml">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>XML</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container mt-5">
+        <div class="row">
+        <div class="col-12">
+            
+            <a href="index.php">
+            <button class="btn btn-info">
+            See JSON version
+            </button>
+            </a>
+            
+        </div>
+        </div>
+    </div>
+	<div class="container">
 		<div class="row">
 			<div class="col-12">
 				<h1>Search for medicine XML</h1>
@@ -96,3 +121,5 @@
 		}
 		?>
 	</div>
+</body>
+</html>
